f7b9ef5399d432d0926730cbf50bc0f3ab45bc7e
Rafaela Paula <24409853+rafaelapaula@users.noreply.github.com>
2022-10-14 00:25:59 -0300

    Ferramentas para redução de boilerplates (#2)
    * add setup_sourcery task
    * create AutoMockable.stencil
    * create AutoMockable.swift
    * add steps to user sourcery
    * update project

---
+++ App/TheMovieDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDatabaseApp.swift
+//  TheMovieDatabase
+//
+//  Created by rafaela.lourenco on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
